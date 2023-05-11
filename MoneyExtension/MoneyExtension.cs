d0fe8a571dcdcfed083fde310bf814f2b83d6699
Igor Santiago <contato.igorsantiago@outlook.com>
2023-05-11 15:01:05 -0300

    Add implementation

---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace MoneyExtension;
+
+public static class Money
+{
+    public static int ToCents(this decimal value)
+    {
+        if (value < 0)
+            return 0;
+
+        var valueString = value.ToString("N2").Replace(",", "").Replace(".", "");
+
+        if (string.IsNullOrEmpty(valueString))
+            return 0;
+
+        int.TryParse(valueString, out var result);
+        return result;
+    }
+}
